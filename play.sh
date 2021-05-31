c4f425e12f5670361a2c85c10649658ae693b8de
Michael Gokhman <michael.gokhman@yahoo.com>
2021-05-31 20:07:00 +0000

    play.sh: use model_2 for python->java, update example

--- play.sh
+++ play.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-cat play/in.py | python ./translate.py --BPE_path data/BPE_with_comments_codes --src_lang python --tgt_lang java --model_path data/model_1.pth > play/out.java
+python ./translate.py --BPE_path data/BPE_with_comments_codes --src_lang python --tgt_lang java --model_path data/model_2.pth < play/in.py > play/out.java
--- play/in.py
+++ play/in.py
@@ -1,2 +1,6 @@
+import numpy as np
+
 def main():
-  print("hello koko")
+  arr = np.array([1,3,5,7,11])
+  np.mean(arr * 2)
+  print("the average is:", avg)
